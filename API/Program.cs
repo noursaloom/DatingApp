081da191e3c76688a77424de2f2a7440b636b5ef
Nour Omar <noordawaymeh@outlook.com>
2023-06-18 22:34:35 +0300

    Creating cliet side of the application

--- API/Program.cs
+++ API/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<DataContext>(opt=>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,7 +19,7 @@ if (app.Environment.IsDevelopment())
 {
 
 }
-
+app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyHeader().WithOrigins("http://localhost:4200"));
 app.MapControllers();
 
 app.Run();
